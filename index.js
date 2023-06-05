6c88b43bdc3615c0d38ae7425608d76d2d739211
Yacine Djenidi <djenidi.dev@gmail.com>
2023-06-05 17:54:28 +0100

    upp

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = process.env.PORT
 const bodyParser = require("body-parser");
 const Botly = require("botly");
 const botly = new Botly({
-    accessToken: pageAccessToken,
-    verifyToken: verificationToken,
-    webHookPath: yourWebHookPath,
+    accessToken: process.env.AT,
+    verifyToken: process.env.VT,
+    webHookPath: process.env.WP,
     notificationType: Botly.CONST.REGULAR,
     FB_URL: 'https://graph.facebook.com/v2.6/'
 });
